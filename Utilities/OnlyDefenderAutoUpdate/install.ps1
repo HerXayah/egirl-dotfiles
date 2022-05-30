a9af511aa84974691ee8782ce1afb62b42032336
PrincessAkira <JustSweetLuna@gmail.com>
2022-05-30 13:53:45 +0200

    Updated Install Script

--- Utilities/OnlyDefenderAutoUpdate/install.ps1
+++ Utilities/OnlyDefenderAutoUpdate/install.ps1
@@ -1,8 +1,8 @@
 #Requires -RunAsAdministrator
 
 Write-Host "Starting Installation..."
-
-Start-Sleep -s 5
+Write-Host "Waiting three seconds to see if you cancel"
+Start-Sleep -s 3
 
 $folder = "C:\Logs"
 $windowsFolder = "C:\Windows"
@@ -10,30 +10,41 @@ $currentFolder = Get-Location
 
 copy $currentFolder\UpdateDefender.cmd $windowsFolder\UpdateDefender.cmd
 
-Write-Host "Creating Task"
-
-$DurationTimeSpanIndefinite = ([TimeSpan]::MaxValue) 
+#$DurationTimeSpanIndefinite = ([TimeSpan]::MaxValue) 
 
 # every 5 hours for Indefinitely
 $trigger = New-ScheduledTaskTrigger `
     -Once `
     -At (Get-Date) `
-    -RepetitionInterval (New-TimeSpan -Hours 5) `
+    -RepetitionInterval (New-TimeSpan -Hours 6) `
     -RepetitionDuration (New-TimeSpan -Days (365 * 68)) `
     -ThrottleLimit 1 `
 # Run task as system
-$principal = New-ScheduledTaskPrincipal -UserId 'NT AUTHORITY\SYSTEM' -RunLevel Highest
+$User= "NT AUTHORITY\SYSTEM"
+$principal = New-ScheduledTaskPrincipal -User $User -RunLevel Highest
 $action = New-ScheduledTaskAction -Execute 'C:\Windows\UpdateDefender.cmd' 
 $settings = New-ScheduledTaskSettingsSet -RunOnlyIfNetworkAvailable -WakeToRun -Hidden -ExecutionTimeLimit (New-TimeSpan -Minutes 5) -MultipleInstances IgnoreNew
 $task = New-ScheduledTask -Action $action -Trigger $trigger -Principal $principal -Settings $settings
+
+
+#if task exists, skip
+if (Get-ScheduledTask -TaskName "UpdateDefender" -ErrorAction SilentlyContinue) {
+    Write-Host "Task already exists"
+    } else {
+    Write-Host "Creating Task"
 Register-ScheduledTask UpdateDefender -InputObject $task
-
-
-
 Enable-ScheduledTask -TaskName 'UpdateDefender'
+}
 
 Write-Host "Creating Folder"
 
-New-Item -Path $folder -ItemType Directory
+# if folder already exist then skip
+if (Test-Path $folder) {
+    Write-Host "Folder already exist. Skippping..."
+} else {
+    New-Item $folder -ItemType Directory
+}
 
 Write-Host "Installation Complete"
+Start-Sleep -s 2
+Write-Host "You can change the Windows Version and set it to indefinite via TaskSchedule.msc"
