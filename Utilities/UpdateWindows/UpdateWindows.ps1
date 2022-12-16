aefdd82613608bc137eddf7d14d54c8e2df53d7a
Sarah <justsweetluna@gmail.com>
2022-12-16 18:31:17 +0100

    meow

---
+++ Utilities/UpdateWindows/UpdateWindows.ps1
@@ -0,0 +1,25 @@
+#Requires -RunAsAdministrator
+
+# check if log file exists
+# if it does not exist, create a new log file
+if (!(Test-Path "C:\Logs\UpdateWindows.log")) {
+    New-Item -ItemType File -Path "C:\Logs\UpdateWindows.log"
+    Write-Output "Initializing Logfile..." | Out-File -FilePath "C:\Logs\UpdateWindows.log" -Append
+}
+
+# if logfile exists add a space line
+if (Test-Path "C:\Logs\UpdateWindows.log") {
+    Write-Output "------------------------" | Out-File -FilePath "C:\Logs\UpdateWindows.log" -Append
+    Write-Output "Running..." | Out-File -FilePath "C:\Logs\UpdateWindows.log" -Append
+}
+
+# for each update in Get-WindowsUpdate create a toast notification and write output to log file
+Get-WindowsUpdate -MicrosoftUpdate | ForEach-Object {
+    New-BurntToastNotification -Text "Installing Update: $($_.Title)" -AppLogo "C:\Windows\BurntToast\logo.png"
+    # add date and time to log
+    Get-Date | Out-File -FilePath "C:\Logs\UpdateWindows.log" -Append
+    Write-Output "Installing Update: $($_.Title)" | Out-File -FilePath "C:\Logs\UpdateWindows.log" -Append
+    Install-WindowsUpdate -MicrosoftUpdate -AcceptAll -Update $_.Identity.UpdateID
+}
+
+    
---
+++ Utilities/UpdateWindows/install.ps1
@@ -0,0 +1,58 @@
+#Requires -RunAsAdministrator
+
+Write-Host "Starting Installation..."
+Write-Host "Waiting three seconds to see if you cancel"
+Start-Sleep -s 3
+
+$folder = "C:\Logs"
+$windowsFolder = "C:\Windows"
+$currentFolder = Get-Location
+$logofolder = "C:\Windows\BurntToast"
+$User= "NT AUTHORITY\SYSTEM"
+
+#if files already exist, skip them
+if(!(Test-Path $windowsFolder\UpdateWindows.ps1)){
+    copy $currentFolder\UpdateDefender.ps1 $windowsFolder\UpdateDefender.ps1
+}
+else {
+
+    Write-Host "Update Windows already exists, skipping..."
+
+}
+
+Write-Host "Installing Software"
+Install-Module -Name BurntToast
+Install-Module PSWindowsUpdate
+Add-WUServiceManager -MicrosoftUpdate
+
+
+#if task exists, skip
+if (Get-ScheduledTask -TaskName "UpdateWindows" -ErrorAction SilentlyContinue) {
+    Write-Host "Task already exists"
+    } else {
+    Write-Host "Creating Task"
+Register-ScheduledTask -Xml (Get-Content 'UpdateWindows.xml' | out-string) -TaskName "UpdateWindows" -User $User
+Enable-ScheduledTask -TaskName 'UpdateWindows'
+}
+
+Write-Host "Creating Folder"
+
+# if folder already exist then skip
+if (Test-Path $folder) {
+    Write-Host "Folder already exist. Skippping..."
+} else {
+    New-Item $folder -ItemType Directory
+}
+
+# if folder and icon already exist then skip
+if (Test-Path $logofolder) {
+    Write-Host "Folder already exist. Skippping..."
+} else {
+    New-Item $logofolder -ItemType Directory
+    copy $currentFolder\logo.png $logofolder\logo.png
+}
+
+
+
+Write-Host "Installation Complete"
+exit
