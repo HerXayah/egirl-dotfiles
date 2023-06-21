8234834f2cc11c6157b89a65c2c94514df3a690a
Sarah Engel <justsweetluna@gmail.com>
2023-06-21 19:18:14 +0200

    update dotfiles

--- Tweaks/Svchost_Tweaker.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-for /f "tokens=2 delims==" %%i in ('wmic os get TotalVisibleMemorySize /format:value') do set mem=%%i
-set /a ram=%mem% + 1024000
-reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control" /v "SvcHostSplitThresholdInKB" /t REG_DWORD /d "%ram%" /f
-cls
-echo Press any key to exit.
-pause > nul
